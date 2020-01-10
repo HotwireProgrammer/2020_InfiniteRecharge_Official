de7878b42745984339d2fd7942bc1049b0162fcf
Zack <zbessler@gmail.com>
2020-01-10 11:32:56 -0800

    Commiting dashboard

---
+++ Dashboard/src/pages/driver.page.vue
@@ -0,0 +1,22 @@
+<template>
+      <v-container class="fill-height" fluid>
+        <PressureButtons />
+        <LauncherPsi />
+      </v-container>
+</template>
+
+<script>
+import PressureButtons from '../components/PressureButtons';
+import LauncherPsi from '../components/LauncherPsi';
+export default {
+  name: 'DriverPage',
+
+  components: {
+    LauncherPsi, 
+    PressureButtons
+  },
+  
+
+  data: () => ({ data: null}),
+};
+</script>
