60cacc14cb18886c9fe0aa0c05756b91af30182d
Zack <zbessler@gmail.com>
2020-01-18 11:27:13 -0800

    removed a bit

--- Dashboard/src/pages/detailed.page.vue
+++ Dashboard/src/pages/detailed.page.vue
@@ -2,22 +2,33 @@
 import PowerChart from '../components/PowerChart';
 import PressureButtons from '../components/PressureButtons';
 import UltrasonicDownCounter from '../components/UltrasonicDownCounter';
+import PDIChart from '../components/PDIChart';
 
 export default {
-  name: 'DetailedPage',
+    name: 'DetailedPage',
 
-  components: {
-    PowerChart, PressureButtons, UltrasonicDownCounter
-  },
+    components: {
+        PowerChart,
+        PressureButtons,
+        UltrasonicDownCounter,
+        PDIChart
+    },
 
-  data: () => ({ data: null}),
+    data: () => ({ data: null}),
 };
 </script>
 
 <template>
-      <v-container class="fill-height" fluid>
-        <PowerChart />
-        <PressureButtons />
-        <UltrasonicDownCounter />
-      </v-container>
+<v-container class="fill-height" fluid>
+    <PowerChart />
+    <PressureButtons />
+    <UltrasonicDownCounter />
+    <PDIChart class="pdi-chart" />
+</v-container>
 </template>
+
+<style scoped lang="scss">>
+.pdi-chart {
+    width: 600px;
+}
+</style>
--- Dashboard/src/pages/driver.page.vue
+++ Dashboard/src/pages/driver.page.vue
@@ -1,19 +1,43 @@
 <script>
-import PDIChart from '../components/PDIChart';
 import { NetworkTables } from '../utils/networktables'
 import * as logger from '../utils/logger'
 
 export default {
     name: 'DriverPage',
 
-    components: {
-        PDIChart
-    },
+    components: {},
 
     data: function() {
         return {
             autoModeValue: 0,
-            colorSelectionValue: 0
+            colorSelectionValue: 0,
+            autoModes: [
+                {
+                    text: 'Basic Mode',
+                    value: 0
+                },{
+                    text: 'Advanced Mode',
+                    value: 1
+                },{
+                    text: 'Super Mode',
+                    value: 2
+                }
+            ],
+            colorModes: [
+                {
+                    text: 'Red',
+                    value: 0
+                },{
+                    text: 'Yellow',
+                    value: 1
+                },{
+                    text: 'Blue',
+                    value: 2
+                },{
+                    text: 'Green',
+                    value: 3
+                }
+            ]
         }
     },
     methods: {
@@ -34,6 +58,10 @@ export default {
             console.log("CLIMB  - ",  true);
             logger.endMatchProcessing();
         },
+        endMatch: function() {
+            console.log("CLIMB  - ",  true);
+            logger.endMatchProcessing();
+        },
         reachUp: function() {
             NetworkTables.putValue('reachUp', true);
             console.log("REACH UP  - ",  true);
@@ -47,74 +75,69 @@ export default {
 </script>
 
 <template>
-    <v-container class="fill-height" fluid>
-        <PDIChart class="pdi-chart" />
-        <div class="auto-container">
-            <v-select
-                :items="[{text: 'Basic Mode', value: 0},{text: 'Advanced Mode', value: 1},{text: 'Super Mode', value: 2}]"
-                label="Auto Mode"
-                v-model="autoModeValue"
-                outlined
-                color="primary"
-                item-color="primary"
-                class="auto-dropdown"
-            ></v-select>
-            <v-btn
-                class="auto-button"
-                outlined
-                color="primary"
-                height="55"
-                @click="startAuto()">Start Auto</v-btn>
-        </div>
-        <div class="color-container">
-            <v-select
-                :items="[{text: 'Red', value: 0},{text: 'Yellow', value: 1},{text: 'Blue', value: 2},{text: 'Green', value: 3}]"
-                label="Select Color"
-                v-model="colorSelectionValue"
-                outlined
-                color="primary"
-                item-color="primary"
-                class="auto-dropdown"
-            ></v-select>
-            <v-btn
-                class="auto-button"
-                outlined
-                color="primary"
-                height="55"
-                @click="setColor()">Set Color</v-btn>
-        </div>
-         <v-btn
-                class="auto-button"
-                outlined
-                color="primary"
-                height="55"
-                @click="spinColorWheel()">Spin Color Wheel</v-btn>
+<v-container class="fill-height" fluid>
+    <div class="auto-container">
+        <v-select
+            :items="autoModes"
+            label="Auto Mode"
+            v-model="autoModeValue"
+            outlined
+            color="primary"
+            item-color="primary"
+            class="auto-dropdown"
+        ></v-select>
         <v-btn
-                class="auto-button"
-                outlined
-                color="primary"
-                height="55"
-                @click="climb()">Climb</v-btn>
-         <v-btn
-                class="auto-button"
-                outlined
-                color="primary"
-                height="55"
-                @click="reachUp()">Reach Up</v-btn>
+            class="auto-button"
+            outlined
+            color="primary"
+            height="55"
+            @click="startAuto()">Start Auto</v-btn>
+    </div>
+    <div class="color-container">
+        <v-select
+            :items="colorModes"
+            label="Select Color"
+            v-model="colorSelectionValue"
+            outlined
+            color="primary"
+            item-color="primary"
+            class="auto-dropdown"
+        ></v-select>
         <v-btn
-                class="auto-button"
-                outlined
-                color="primary"
-                height="55"
-                @click="toggleIntake()">Toggle Intake</v-btn>
-    </v-container>
+            class="auto-button"
+            outlined
+            color="primary"
+            height="55"
+            @click="setColor()">Set Color</v-btn>
+    </div>
+        <v-btn
+            class="auto-button"
+            outlined
+            color="primary"
+            height="55"
+            @click="spinColorWheel()">Spin Color Wheel</v-btn>
+    <v-btn
+            class="auto-button"
+            outlined
+            color="primary"
+            height="55"
+            @click="climb()">Climb</v-btn>
+        <v-btn
+            class="auto-button"
+            outlined
+            color="primary"
+            height="55"
+            @click="reachUp()">Reach Up</v-btn>
+    <v-btn
+            class="auto-button"
+            outlined
+            color="primary"
+            height="55"
+            @click="toggleIntake()">Toggle Intake</v-btn>
+</v-container>
 </template>
 
 <style scoped lang="scss">
-
-    .pdi-chart {
-        width: 600px;
-    }
 
     .auto-button {
         margin-left: 20px;
